db82b97a5d004cfd2ab37c9906dd389872fcbcdd
Zane Ravenholt <zravenholt@gmail.com>
2017-09-21 12:02:05 -0700

    react up and running

---
+++ client/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+  render () {
+    return (
+      <div>hello from react</div>
+    );
+  }
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ let express = require('express');
 let app = express();
 let path = require('path');
 let PORT = process.env.PORT || 9001;
+let bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/bundles', express.static(path.join(__dirname, '../bundles')));
+
 
 app.listen(PORT, function() {
   console.log('PERSONAL: connection established at: ', PORT);
